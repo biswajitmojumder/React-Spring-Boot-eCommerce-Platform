05654295a29e24047c7e57279749078c9df7f204
kanastasov <kirilanastasoff@gmail.com>
2022-03-05 08:50:56 +0200

    Login bootstrap

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import {Form,Button, Col} from 'react-bootstrap'
+import {Link,useParams,useSearchParams } from 'react-router-dom'
 
 const LoginScreen = () => {
   return (
     <div>
-    <h1>  LoginScreen</h1>
+        <Form>
+        <h1>Login </h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Login
+        </Button>
+        <hr/>
+
+        <Form.Group className="mb-3" controlId="formBasicRegister">
+            <Form.Label>Not a member? <Link to={`/register/`}>Sign up
+                             </Link></Form.Label>
+                   
+        </Form.Group>
+        </Form>
     
     
     </div>
